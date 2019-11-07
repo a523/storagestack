96aabd688fae1cf5eef4415733d3b2a542b1c218
lei xin <chjgreen@qq.com>
2019-11-07 16:52:31 +0800

    返回字符改成OK

--- Agent/tests/functional_test.py
+++ Agent/tests/functional_test.py
@@ -6,4 +6,4 @@ import requests
 def test_server_status():
     resp = requests.get('http://localhost:8600/hello')
     assert resp.status_code == 200, 'Agent web server unavailable'
-    assert resp.text == 'Hello, World'
+    assert resp.text == 'OK'
