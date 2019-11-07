4b73e5a2e32f5b61a51c26226eabcc9a7fe7fc72
lei xin <chjgreen@qq.com>
2019-11-07 17:37:11 +0800

    增加请求Agent接口示例

--- ControlServer/deploy/views.py
+++ ControlServer/deploy/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import requests
 
-# Create your views here.
+
+# def gen_node_agent_url(ip):
+
+@api_view(['GET'])
+def check_conn(request):
+    """检查node上的Agent是否可以连通"""
+    ip = request.GET['ip']
+    resp = requests.get("http://{}:{}/hello".format(ip, 8600))
+    if resp.ok:
+        return Response('ok')
