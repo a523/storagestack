61eb06ec65eb6aa8458a0d84585f150c89816eb6
xin <chjgreen@qq.com>
2019-11-17 18:58:35 +0800

    增加 错误类型

--- ControlServer/ControlServer/errors.py
+++ ControlServer/ControlServer/errors.py
@@ -11,8 +11,21 @@ class RequestAgentError(ControlServerError):
         self.args = args
 
     def __str__(self):
-        return "Failed to request agent ({0}: {1}), code: {2}, message: {3}, args: {4}".format(self.method
-                                                                                               , self.resp.url,
-                                                                                               self.resp.status_code,
-                                                                                               self.resp.text,
-                                                                                               self.args)
+        return "Failed to request agent ({0}: {1}), " \
+               "code: {2}, message: {3}, args: {4}".format(self.method
+                                                           , self.resp.url,
+                                                           self.resp.status_code,
+                                                           self.resp.text,
+                                                           self.args)
+
+
+class TaskException(Exception):
+    """任务在Node上执行时出错"""
+
+    def __init__(self, node, task):
+        super().__init__(node, task)
+        self.node = node
+        self.task = task
+
+    def __str__(self):
+        return "Failed to '{0}' on node {1}".format(self.task.name, self.node)
