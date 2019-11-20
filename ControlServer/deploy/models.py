4744ce335072bfce323450068c1d50a205aac59c
xin <chjgreen@qq.com>
2019-11-20 21:30:36 +0800

    修改models

--- ControlServer/deploy/models.py
+++ ControlServer/deploy/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 
 class Nodes(models.Model):
-    ip = models.GenericIPAddressField()
+    ip = models.GenericIPAddressField(unique=True)
     create_time = models.DateTimeField(auto_created=True)
