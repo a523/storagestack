f9d8aa5c5f921be05e99c9ca9be53962d5409589
xin <chjgreen@qq.com>
2019-11-23 20:14:31 +0800

    修改model 创建时间bug

--- ControlServer/deploy/models.py
+++ ControlServer/deploy/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Nodes(models.Model):
     ip = models.GenericIPAddressField(unique=True)
-    create_time = models.DateTimeField(auto_created=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
