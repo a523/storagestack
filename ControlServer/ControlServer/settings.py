04545e499b99499325a7696a4ae9f8fd65e54b72
xin <chjgreen@qq.com>
2019-12-04 17:28:00 +0800

    添加测试服务器白名单

--- ControlServer/ControlServer/settings.py
+++ ControlServer/ControlServer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'au%k-5s9l+pidaad*c!8fyu$p3g_427nid@&quvc7%i)$)sg)k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['testserver']
 
 
 # Application definition
