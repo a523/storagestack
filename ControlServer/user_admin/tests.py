4361638210481a996d2fafdaa0529a20060cf87f
xin <chjgreen@qq.com>
2019-12-04 18:10:10 +0800

    添加创建用户的测试用例

--- ControlServer/user_admin/tests.py
+++ ControlServer/user_admin/tests.py
@@ -7,3 +7,22 @@ class UserTestCase(TestCase):
         resp = self.client.get(reverse('user_admin:users_list'))
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(isinstance(resp.json(), list))
+
+    def test_create_user(self):
+        user_info = {'user_name': 'xin', 'password': 'test_a!'}
+        resp = self.client.post(reverse('user_admin:users_list'), user_info)
+        self.assertEqual(resp.status_code, 201)
+        new_user_info = resp.json()
+        user_id = new_user_info['id']
+        self.assertTrue(isinstance(new_user_info, dict))
+        self.assertTrue(isinstance(user_id, int))
+        self.assertFalse(new_user_info['is_superuser'], "设计应不能通过web创建超级用户")
+
+    def test_get_user_detail(self):
+        pass
+
+    def test_update_user(self):
+        pass
+
+    def delete_user(self):
+        pass
