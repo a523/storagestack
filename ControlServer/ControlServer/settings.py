f5c73aea21801595aa75a873b8031f32edb74cd4
xin <chjgreen@qq.com>
2019-12-06 21:22:13 +0800

    补充传统认证方式for drf 文档登录

--- ControlServer/ControlServer/settings.py
+++ ControlServer/ControlServer/settings.py
@@ -153,6 +153,8 @@ LOGGING = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
