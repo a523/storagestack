150f914192da442235370a54e9b317b21320f75c
xin <chjgreen@qq.com>
2019-12-15 16:42:42 +0800

    修复权限验证

--- ControlServer/user_admin/permissions.py
+++ ControlServer/user_admin/permissions.py
@@ -9,11 +9,13 @@ class ActionPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         method = request.method.lower()
         view_permission = getattr(view, method).permission
+        view_permission_key = (view.__module__.split('.')[0],) + (view_permission['codename'],)
         if not view_permission:
             return True
         else:
-            user_permissions = request.user.get_all_action_permissions
-            if view_permission in user_permissions:
+            user_permissions = request.user.get_all_action_permissions()
+            user_permissions_key = {p.natural_key() for p in user_permissions}
+            if view_permission_key in user_permissions_key:
                 return True
             else:
                 return False
