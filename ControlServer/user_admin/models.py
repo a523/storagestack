d886bd421d09a73e2712e2c46187eb7376e92ff3
xin <chjgreen@qq.com>
2019-12-15 18:13:30 +0800

    重构 user model

--- ControlServer/user_admin/models.py
+++ ControlServer/user_admin/models.py
@@ -1,9 +1,45 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth.models import Group, AbstractUser
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import Group as AuthGroup
+
+
+class ActionPermission(models.Model):
+    """
+    事件权限模型
+    """
+    GET = 'GET'
+    POST = 'POST'
+    PUT = 'PUT'
+    DELETE = 'DELETE'
+    METHOD = [
+        (GET, "get"),
+        (POST, "post"),
+        (PUT, "put"),
+        (DELETE, "delete"),
+    ]
+    app = models.CharField(_('app'), max_length=100)
+    codename = models.CharField(_('codename'), max_length=100)
+    view = models.CharField(_('view'), max_length=100)
+    method = models.CharField(_('method'), choices=METHOD, max_length=8)
+    desc = models.CharField(_('desc'), max_length=255)
+
+    class Meta:
+        verbose_name = _('action_permission')
+        verbose_name_plural = _('action_permission')
+        unique_together = (('app', 'codename'),)
+        ordering = ('app', 'view', 'codename')
+
+    def __str__(self):
+        return '%s | %s' % (self.app, self.codename)
+
+    def natural_key(self):
+        return (self.app,) + (self.codename,)
 
 
 class User(AbstractUser):
+    action_permissions = models.ManyToManyField(ActionPermission, verbose_name=_('action_permissions'), blank=True, )
+
     def get_user_action_permissions(self):
         if not self.is_active or self.is_anonymous:
             return set()
@@ -29,38 +65,9 @@ class User(AbstractUser):
         }
 
 
-class ActionPermission(models.Model):
-    """
-    事件权限模型
-    """
-    GET = 'GET'
-    POST = 'POST'
-    PUT = 'PUT'
-    DELETE = 'DELETE'
-    METHOD = [
-        (GET, "get"),
-        (POST, "post"),
-        (PUT, "put"),
-        (DELETE, "delete"),
-    ]
-    app = models.CharField(_('app'), max_length=100)
-    codename = models.CharField(_('codename'), max_length=100)
-    view = models.CharField(_('view'), max_length=100)
-    method = models.CharField(_('method'), choices=METHOD, max_length=8)
-    desc = models.CharField(_('desc'), max_length=255)
-    users = models.ManyToManyField(User, verbose_name=_('users'), related_name='action_permissions',
-                                   related_query_name='action_permission', blank=True, )
-    groups = models.ManyToManyField(Group, verbose_name=_('groups'), related_name='action_permissions',
-                                    related_query_name='action_permission', blank=True, )
-
-    class Meta:
-        verbose_name = _('action_permission')
-        verbose_name_plural = _('action_permission')
-        unique_together = (('app', 'codename'),)
-        ordering = ('app', 'view', 'codename')
-
-    def __str__(self):
-        return '%s | %s' % (self.app, self.codename)
-
-    def natural_key(self):
-        return (self.app,) + (self.codename,)
+class Group(AuthGroup):
+    action_permissions = models.ManyToManyField(
+        ActionPermission,
+        verbose_name=_('action_permissions'),
+        blank=True,
+    )
--- ControlServer/user_admin/serializers.py
+++ ControlServer/user_admin/serializers.py
@@ -1,10 +1,20 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from user_admin.models import ActionPermission
 
 User = get_user_model()
 
 
+class ActionPermissionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ActionPermission
+        fields = ('app', 'codename', 'desc')
+        read_only_fields = ('app', 'codename', 'desc')
+
+
 class UserSerializer(serializers.ModelSerializer):
+    action_permissions = ActionPermissionSerializer(many=True)
+
     class Meta:
         model = User
         fields = '__all__'
